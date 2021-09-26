599b1ba29a58fb1434f226be9d36065c3910477a
Alvaro Esparza <you@example.com>
2021-09-26 17:23:21 -0400

    "Completed task 4. Operators"

--- aesparza98.github.io/javascript/external.js
+++ aesparza98.github.io/javascript/external.js
@@ -27,3 +27,10 @@ value "sure"*/
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
 */
+
+//Operators
+const x = 22;
+const y =33;
+document.writeln(x + y) ;
+document.writeln("<br>");
+document.writeln(x += 33);
