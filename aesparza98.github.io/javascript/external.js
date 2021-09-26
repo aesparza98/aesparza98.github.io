9962f7f5eb99f262d59095059ab283119748b3f2
Alvaro Esparza <you@example.com>
2021-09-26 17:15:52 -0400

    "Completed Task 3"

--- aesparza98.github.io/javascript/external.js
+++ aesparza98.github.io/javascript/external.js
@@ -1,5 +1,29 @@
 //document.write("Where is my JS code?");
 //window.alert(5 + 6);
 //window.alert('5 + 6');
-document.write('<hl>A heading</hl>');
-document.write('<p>A sentence.</p>');
+//document.write('<hl>A heading</hl>');
+//document.write('<p>A sentence.</p>');
+/*const x = "web";
+const y = "mapping";
+const out = x + y;
+document.write(out);*/
+
+//A prompt box is used to prompt users to input a value before entering a page.
+/*user_name = window.prompt("Please enter your name","Type your name here");
+document.write(user_name);*/
+
+/* Remove this tag for excercise 2 to run
+
+// there are many ways to use the prompt feature
+sign = window.prompt(); // open the blank prompt window
+sign = prompt(); // open the blank prompt window
+sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
+sign = window.prompt('Are you feeling lucky','sure'); /*
+open the window with Text "Are you feeling lucky" and default
+value "sure"*/
+
+/* Remove this tag for excercise 3 to run
+// A prompt box is used to prompt users to input a value before entering a page
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
+*/
