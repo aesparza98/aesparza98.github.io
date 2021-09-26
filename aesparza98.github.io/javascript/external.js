de10669c0f6955515733fd547c7a8d9122db6fc1
Alvaro Esparza <you@example.com>
2021-09-26 12:55:24 -0400

    "Completed task 2. Output"

--- aesparza98.github.io/javascript/external.js
+++ aesparza98.github.io/javascript/external.js
@@ -1,1 +1,5 @@
-document.write("Where is my JS code?");
+//document.write("Where is my JS code?");
+//window.alert(5 + 6);
+//window.alert('5 + 6');
+document.write('<hl>A heading</hl>');
+document.write('<p>A sentence.</p>');
