11db8bdc50197562dd0e71377891305eb787f02d
Alvaro Esparza <you@example.com>
2021-10-03 17:57:14 -0400

    "Defined a function for the welcome message"

--- javascript/Lab3.js
+++ javascript/Lab3.js
@@ -8,8 +8,9 @@ function welcome()
   let b = "Type your name here.";
 
 //A prompt box is used to prompt users to input a value before entering a page
-user_name = window.promp(a,b);
+user_name = window.prompt(a,b);
 message = "<h1>Hello, welcome to my webpage," + user_name + "!</h1>"
 
 return message
 }
+document.write(welcome());
